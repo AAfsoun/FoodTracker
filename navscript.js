5e0bfecd0bc3fee29877cc073c10c14e23ce6189
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2022-07-05 20:36:17 -0700

    Add dynamic nav bar

---
+++ navscript.js
@@ -0,0 +1,23 @@
+// @ts-check
+/*
+ Code to be used with all files in web page
+*/
+
+
+/**
+ * Put the nav bar html text into the HTML
+ */
+let navHtmlText = '<ul>' +
+              '<li><a class="active" href="websitecode.html">Home</a></li>' +
+              '<li><a href="recipes.html">MyRecipes </a></li>' +
+              '<li><a href="#">AllRecipes </a></li>' +
+              '<li><a href="#">AboutUs </a></li>' +
+              '<li><a href="#">ContactUs </a></li>' +
+              '</ul>'
+document.getElementById("nav").innerHTML = navHtmlText;
+
+
+
+
+
+
